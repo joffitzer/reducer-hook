717c97aa1f99b5a84584a667af6f3e0b9ddd5d4d
joffitzer <joffitzer@gmail.com>
2020-04-20 11:21:29 -0400

    using state object to keep track of multiple counters

--- src/components/CounterTwo.js
+++ src/components/CounterTwo.js
@@ -1,17 +1,26 @@
 import React, {useReducer} from 'react'
 
 const initialState = {
-    firstCounter: 0
+    firstCounter: 0,
+    secondCounter: 10
 }
 const reducer = (state, action) => {
     switch(action.type) {
         case 'increment':
             return {
-                firstCounter: state.firstCounter + action.value
+                ...state, firstCounter: state.firstCounter + action.value
             }
         case 'decrement':
             return {
-                firstCounter: state.firstCounter - action.value
+                ...state, firstCounter: state.firstCounter - action.value
+            }
+        case 'increment2':
+            return {
+                ...state, secondCounter: state.secondCounter + action.value
+            }
+        case 'decrement2':
+            return {
+                ...state, secondCounter: state.secondCounter - action.value
             }
         case 'reset': 
             return initialState
@@ -26,9 +35,12 @@ function CounterTwo() {
 
     return(
         <div>
-            <div>Count - {count.firstCounter}</div>
+            <div>First Counter - {count.firstCounter}</div>
+            <div>Second Count - {count.secondCounter}</div>
             <button onClick={() => dispatch({type: 'increment', value: 1})}>Increment</button>
             <button onClick={() => dispatch({type: 'decrement', value: 1})}>Decrement</button>
+            <button onClick={() => dispatch({type: 'increment2', value: 1})}>Increment Counter Two</button>
+            <button onClick={() => dispatch({type: 'decrement2', value: 1})}>Decrement counter Two</button>
             <button onClick={() => dispatch({type: 'increment', value: 5})}>Increment by 5</button>
             <button onClick={() => dispatch({type: 'decrement', value: 5})}>Decrement by 5</button>
             <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
