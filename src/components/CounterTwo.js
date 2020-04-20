6a8613ad38a9f8092f4d6ffeb5e0b3a6bf5d6665
joffitzer <joffitzer@gmail.com>
2020-04-20 11:17:58 -0400

    using multiple properties in reducer function - action.type and action.value

--- src/components/CounterTwo.js
+++ src/components/CounterTwo.js
@@ -7,11 +7,11 @@ const reducer = (state, action) => {
     switch(action.type) {
         case 'increment':
             return {
-                firstCounter: state.firstCounter + 1
+                firstCounter: state.firstCounter + action.value
             }
         case 'decrement':
             return {
-                firstCounter: state.firstCounter - 1
+                firstCounter: state.firstCounter - action.value
             }
         case 'reset': 
             return initialState
@@ -27,8 +27,10 @@ function CounterTwo() {
     return(
         <div>
             <div>Count - {count.firstCounter}</div>
-            <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
-            <button onClick={() => dispatch({type: 'decrement'})}>Decrement</button>
+            <button onClick={() => dispatch({type: 'increment', value: 1})}>Increment</button>
+            <button onClick={() => dispatch({type: 'decrement', value: 1})}>Decrement</button>
+            <button onClick={() => dispatch({type: 'increment', value: 5})}>Increment by 5</button>
+            <button onClick={() => dispatch({type: 'decrement', value: 5})}>Decrement by 5</button>
             <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
         </div>
     )
